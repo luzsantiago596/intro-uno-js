e2a77cbeb9f0667afa572ce621ed91dd4ec9a298
Luz Santiago <luz7282@gmail.com>
2023-01-02 14:12:10 -0300

    Ejercicio 3
    Ejercicio 3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import * as heroes from '../../heroes.js';
 
+//Punto 1
 const getHeroeByIdAsync = function(){
     const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
     return new Promise((resolve, reject) => {
@@ -13,3 +14,30 @@ const getHeroeByIdAsync = function(){
         
     })
 };
+
+//Punto 2
+fetch('https://api.github.com/users/manishmshiva')
+    .then(response =>{
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    })
+
+
+//Punto 3
+const getName = async() => {
+    try {
+        const respuesta = await fetch ('https://api.github.com/users/manishmshiva');
+        console.log(respuesta);
+
+        const datos = await respuesta.json();
+        console.log(datos.name);
+ 
+    }catch(error){
+        console.log(error);
+    }
+}
